6c0c4d115e0795b8aab90f7edf089ae19c3c44ae
niephox <rezutasos@gmail.com>
2019-09-18 10:54:23 +0300

    Init commit, root Database connection, basic file structure , basic html css structure.

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700&display=swap&subset=greek" rel="stylesheet">
+
+    <!-- Custom Stylesheets -->
+    <link rel="stylesheet" href="./stylesheets/index.css">
+    <link rel="stylesheet" href="./stylesheets/elements.css">
+    <title>Band Name</title>
+</head>
+<body>
+    <nav class="navbar">
+        <li class="brand"> Band Name  </li>
+        <div class="items">
+          <li class="navItem">About us</li>
+          <li class="navItem">Merchandise</li>
+          <li class="navItem">Contact Us</li>
+        </div>
+      </nav>
+   <section class="hero">
+       <h2>Hero</h2>
+   </section>
+   <section class="cta">
+        <h3>CTA</h3>
+   </section>
+
+    <?php include '../src/helpers/db.php'?>
+</body>
+</html>
---
+++ src/helpers/db.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "band_db";
+require 'console.php';
+$conn = new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+    die("connection Failed " . $conn->connect_error);
+    consoleLog("Connection Failed.");
+}
+
+consoleLog("connected");
+
+$sql = "SELECT * FROM info";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+       
+    }
+} else {
+   consoleLog(" 0 results");
+}
+$conn->close();
+
+?>
