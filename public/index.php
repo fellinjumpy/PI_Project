d6ac62df4711083356e9c3a6efa4e0b460ba1ea3
niephox <rezutasos@gmail.com>
2019-09-19 19:51:19 +0300

    Layout changes

--- public/index.php
+++ public/index.php
@@ -11,6 +11,7 @@
     <!-- Custom Stylesheets -->
     <link rel="stylesheet" href="./stylesheets/index.css">
     <link rel="stylesheet" href="./stylesheets/elements.css">
+    <script src="https://kit.fontawesome.com/d50f80a898.js" crossorigin="anonymous"></script>
     <title>Band Name</title>
 </head>
 
@@ -21,19 +22,40 @@
         <nav>
             <li><a href="#aboutUs">About us</a></li>
             <li><a href="./shop.php">Merchandise</a></li>
-            <li><a href="">Contact us</a></li>
+            <li><a href="#footer">Contact us</a></li>
         </nav>
     </header>
     <!-- Hero Landing -->
-    <section class="hero">
-        <div class="background-image"></div>
+    <section class="hero size100">
+        <div class="background-image main"></div>
     </section>
 
+    <!-- TOURS SHOWS -->
+    <section class="cta" id="aboutUs">
+        <img  src="https://images.unsplash.com/photo-1471958680802-1345a694ba6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1132&q=80" class="cta-image">
+        <div class="cta-content">
+            <h3 class="heading-m">Tours and Shows</h3>
+            <p class="cta-p">Touring and giging in lots of different places
+                is our thing! Wanna see us up close?! Check out 
+                our tour and show dates!
+            </p>
+            <a class="btn" href="./tours.php">Read more</a>
+        </div>
+    </section>
+    <!-- History and members -->
     <section class="cta">
-        <h3>CTA</h3>
+        <img src="https://images.unsplash.com/photo-1518911710364-17ec553bde5d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80" class="second cta-image ">
+        <div class="cta-content align-s">
+            <h3 class="heading-m">Our story.</h3>
+            <p class="cta-p">You up for more details?.Want to read where-how-why we started
+                this band and the whole story behind it? Want to read more about each and everyone of us?
+                This is the place to do it!
+            </p>
+            <a class="btn" href="./history.html">Read more</a>
+        </div>
     </section>
 
-    <section class="aboutUs" id="aboutUs">
+    <!-- <section class="aboutUs" id="aboutUs">
             <h3 class="title">About us</h3>
             <p>Wanna know more about us? See below! </p>
             <hr>
@@ -60,11 +82,11 @@
                 <p>Our past and upcoming shows from small to huge! Check the dates here! </p>
               </li>
             </ul>
-          </section>
+          </section> -->
     <!-- CONTACT SECTION -->
     <section class="contact">
         <h3 class="title">Learn more</h3>
-        <p>Want to know about our upcoming shows and tours? Just sign up for our
+        <p>Want to be informed everytime something new comes up? Just sign up for our
             mailing list.No spam from us! Except if you are a true fan!
             </p>
         <hr>
@@ -78,17 +100,21 @@
             <input type="email" name="email" placeholder="Email">
             <input class="btn" type="submit" name="submit"> </input>
         </form>
-        
     </section>
     <!-- FOOTER  -->
-    <footer>
-        <p>Images courtesy of <a href="http://unsplash.com/">unsplash</a>.</p>
-        <p>Copyright paragraph of who what and how</p>
-        <ul>
-            <li><a href="#"><i class="fa fa-twitter-square fa-2x"></i></a></li>
-            <li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
-            <li><a href="#"><i class="fa fa-snapchat-square fa-2x"></i></a></li>
-        </ul>
+    <footer id="footer">
+        <div class="footer-contact">
+            <h3>Band Name</h3>
+            
+            <ul>
+                <li><a href="#"><i class="fab fa-twitter "></i> </a></li>
+                <li><a href="#"><i class="fab fa-facebook "></i> </a></li>
+                <li><a href="#"><i class="fab fa-snapchat "></i> </a></li>
+            </ul>     
+        </div>
+        <div class="footer-rights">
+            <p>Images courtesy of <a href="http://unsplash.com/">unsplash</a>.</p>
+        </div>
     </footer>
      
     
